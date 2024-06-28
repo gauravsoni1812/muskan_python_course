c23aa5e168a10359583565bf3b98f0e0ce481b73
gauravsoni1812 <gauravsoni8800@gmail.com>
2024-06-28 21:01:35 +0530

    cel_fah

---
+++ cel_to_faren.py
@@ -0,0 +1,9 @@
+cel =  float(input("write the temperature in celcius "))
+iFar = float(input("write the temperature in Farenheit "))
+
+far = (cel*1.8) +32;
+
+cel_fah = (iFar-32) * 1.8
+
+print("The value of celcius in farenheit is", far)
+print("The value of farenheit in celcius is", cel_fah)
