b69451ad8c2705ebde6b948ab5863367fbc6d2ff
gauravsoni1812 <gauravsoni8800@gmail.com>
2024-06-28 20:39:46 +0530

    area of circle

---
+++ class2.py
@@ -0,0 +1,42 @@
+# operators and operands
+
+# arithmetic operations
+# a =12
+# b =11
+# print(a+b)  #adition
+# print(a-b)  #sustraction
+# print(a*b)  #multipication
+# print(a/b)  #division
+# print(a//b) #floor division
+# print(a%b)  #modulus
+# print(a**b) #exponent
+
+
+#assignment operators
+#compound operators
+# a = 12;
+# # a = a+10;
+
+# a += 10
+
+# a -= 10
+
+# a /= 10 #conversion
+
+# a += 12
+
+# a //= 10
+
+# print(15//12);
+
+#relational operators
+
+a=12
+b=45
+
+c=a>b;
+d=b>a;
+e=a>=b
+f=a<=b
+
+print(12==12)
---
+++ homework.py
@@ -0,0 +1,10 @@
+r = input("Enter the radius here in cm ")
+
+r = float(r)  # at this line r is converted to decimal number
+
+p = 2 * 3.14 *r #this is the perimeter
+a = 3.14 * r**2 #this is the area of a cercle
+
+print("The perimeter of cercle is ",p)
+print("The area of cercle is ", a)
+
